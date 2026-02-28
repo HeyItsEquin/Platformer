da16b063dff903f0391ef5bd011e709ec6840a4e
HeyItsEquin <theoriginalthemor@gmail.com>
2026-02-28 14:10:56 -0800

    Implementd animations

--- player.gd
+++ player.gd
@@ -9,6 +9,16 @@ const GRAVITY = 980
 
 var is_jumping = false
 
+enum JumpState { NONE, TAKEOFF, RISING, FALLING, LANDING }
+var jump_state = JumpState.NONE
+
+const TAKEOFF_FRAMES = 4
+const LANDING_FRAMES = 5
+var state_timer = 0
+
+func _process(delta: float) -> void:
+    update_animation()
+
 func _physics_process(delta: float) -> void:
     apply_gravity(delta)
     process_input()
@@ -24,6 +34,32 @@ func apply_gravity(delta: float) -> void:
         gravity_this_frame *= fall_gravity_multiplier
 
     velocity.y += gravity_this_frame * delta
+
+func update_animation() -> void:
+    match jump_state:
+        JumpState.TAKEOFF:
+            state_timer -= 1
+            if state_timer <= 0:
+                jump_state = JumpState.RISING
+                $PlayerSprite.play("jump_rising")
+        JumpState.RISING:
+            if velocity.y >= 0:
+                jump_state = JumpState.FALLING
+                $PlayerSprite.play("jump_falling")
+        JumpState.FALLING:
+            if is_on_floor():
+                jump_state = JumpState.LANDING
+                state_timer = LANDING_FRAMES
+                $PlayerSprite.play("jump_landing")
+        JumpState.LANDING:
+            state_timer -= 1
+            if state_timer <= 0:
+                jump_state = JumpState.NONE
+        JumpState.NONE:
+            if velocity.x != 0:
+                $PlayerSprite.play("moving");
+            else:
+                $PlayerSprite.play("idle")
 
 func process_input() -> void:
     if Input.is_action_pressed("move_right"):
@@ -43,6 +79,9 @@ func jump() -> void:
     if is_on_floor():
         velocity.y = -jump_force
         is_jumping = true
+        jump_state = JumpState.TAKEOFF
+        state_timer = TAKEOFF_FRAMES
+        # $PlayerSprite.play("jump_takeoff")
 
 func jump_cut() -> void:
     if is_jumping and velocity.y < 0:
@@ -51,8 +90,10 @@ func jump_cut() -> void:
 func move(dir: MoveDir) -> void:
     if dir == MoveDir.RIGHT:
         velocity.x = move_speed
+        $PlayerSprite.flip_h = false
     if dir == MoveDir.LEFT:
         velocity.x = -move_speed
+        $PlayerSprite.flip_h = true
     if dir == MoveDir.NONE:
         velocity.x = 0
 

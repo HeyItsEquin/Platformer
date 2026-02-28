75aef2d0b2a2fb182f958d6c371c2993d251a545
HeyItsEquin <theoriginalthemor@gmail.com>
2026-02-28 13:31:13 -0800

    Created basic player movement

--- player.gd
+++ player.gd
@@ -1,17 +1,63 @@
-extends Node2D
+extends CharacterBody2D
 
-var sprite
+@export var move_speed = 400
+@export var jump_force = 500
+@export var jump_cut_multiplier = 0.5
+@export var fall_gravity_multiplier = 1.8
 
-var moveSpeed = 8
+const GRAVITY = 980
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	sprite = get_node("PlayerSprite")
+var is_jumping = false
 
+func _physics_process(delta: float) -> void:
+    apply_gravity(delta)
+    process_input()
+    move_and_slide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("move_right"):
-		position.x += moveSpeed
-	if Input.is_action_pressed("move_left"):
-		position.x -= moveSpeed
+func apply_gravity(delta: float) -> void:
+    if is_on_floor():
+        is_jumping = false
+        return
+
+    var gravity_this_frame = GRAVITY
+    if velocity.y > 0:
+        gravity_this_frame *= fall_gravity_multiplier
+
+    velocity.y += gravity_this_frame * delta
+
+func process_input() -> void:
+    if Input.is_action_pressed("move_right"):
+        move(MoveDir.RIGHT)
+    elif Input.is_action_pressed("move_left"):
+        move(MoveDir.LEFT)
+    else:
+        move(MoveDir.NONE)
+
+    if Input.is_action_just_pressed("move_up"):
+        jump()
+
+    if Input.is_action_just_released("move_up"):
+        jump_cut()
+
+func jump() -> void:
+    if is_on_floor():
+        velocity.y = -jump_force
+        is_jumping = true
+
+func jump_cut() -> void:
+    if is_jumping and velocity.y < 0:
+        velocity.y *= jump_cut_multiplier
+
+func move(dir: MoveDir) -> void:
+    if dir == MoveDir.RIGHT:
+        velocity.x = move_speed
+    if dir == MoveDir.LEFT:
+        velocity.x = -move_speed
+    if dir == MoveDir.NONE:
+        velocity.x = 0
+
+enum MoveDir {
+	RIGHT = 0,
+	LEFT,
+    NONE
+}
